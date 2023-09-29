69650bf9b37b86bd03a2e75cffc89aadc8701ae2
Tyuiu.ChebulaevAG@outlook.com <andrey.chebulaev@gmail.com>
2023-09-29 22:34:41 +0500

    Task4

---
+++ Tyuiu.ChebulaevAG.Sprint1.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChebulaevAG.Sprint1.Task4.V23.Lib;
+
+namespace Tyuiu.ChebulaevAG.Sprint1.Task4.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 3.0;
+            double wait = 2.236;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res, 0.001);
+        }
+    }
+}
---
+++ Tyuiu.ChebulaevAG.Sprint1.Task4.V23/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChebulaevAG.Sprint1.Task4.V23.Lib;
+
+namespace Tyuiu.ChebulaevAG.Sprint1.Task4.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чебулаев А. Г. | РПСб-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Чебулаев Андрей Геннадьевич | РПСб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("* Формула: √|x+y|/|3−x|                                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение x:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.Calculate(x, y);
+            Console.WriteLine($"Результат вычисления формулы: {result:F3}");
+
+            Console.ReadLine();
+        }
+    }
+}
